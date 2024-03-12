db4990d135b65ed6f7fc962806321115bdaa7197
semihbkgr <buyukgungorsemih@gmail.com>
2024-03-13 00:29:18 +0300

    wire up stdout and stderr

--- app/main.go
+++ app/main.go
@@ -8,16 +8,16 @@ import (
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
 	if err != nil {
 		fmt.Printf("Err: %v", err)
 		os.Exit(1)
 	}
-
-	fmt.Println(string(output))
 }
