05d648dca8e9a25958589dec336d5e6de289143b
semihbkgr <buyukgungorsemih@gmail.com>
2024-03-14 23:54:51 +0300

    isolate fs using chroot

--- app/main.go
+++ app/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path"
+	"syscall"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -11,13 +13,50 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
+	err := isolateFS()
+	if err != nil {
+		fmt.Printf("error on isolating fs: %v", err)
+		os.Exit(255)
+	}
+
 	cmd := exec.Command(command, args...)
+	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
-		fmt.Printf("Err: %v", err)
 		os.Exit(cmd.ProcessState.ExitCode())
 	}
+}
+
+func isolateFS() error {
+	dir, err := os.MkdirTemp("", "my_docker_fs_*")
+	if err != nil {
+		return err
+	}
+
+	err = os.Chmod(dir, 0777)
+	if err != nil {
+		return err
+	}
+
+	binPath := "/usr/local/bin"
+
+	err = os.MkdirAll(path.Join(dir, binPath), 0777)
+	if err != nil {
+		return err
+	}
+
+	err = os.Link(binPath, path.Join(dir, binPath))
+	if err != nil {
+		return err
+	}
+
+	err = syscall.Chroot(dir)
+	if err != nil {
+		return err
+	}
+
+	return os.Chdir("/")
 }
