020ef6488a7d61c621bfa591bb279680dcfc9b0c
semihbkgr <buyukgungorsemih@gmail.com>
2024-03-15 00:20:35 +0300

    set pid ns in proc attr

--- app/main.go
+++ app/main.go
@@ -13,50 +13,50 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
-	err := isolateFS()
+	fsDir, err := isolatedFS()
 	if err != nil {
 		fmt.Printf("error on isolating fs: %v", err)
 		os.Exit(255)
 	}
 
 	cmd := exec.Command(command, args...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Chroot:     fsDir,
+		Cloneflags: syscall.CLONE_NEWPID,
+	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	err = cmd.Run()
 	if err != nil {
+		fmt.Println(err)
 		os.Exit(cmd.ProcessState.ExitCode())
 	}
 }
 
-func isolateFS() error {
+func isolatedFS() (string, error) {
 	dir, err := os.MkdirTemp("", "my_docker_fs_*")
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	err = os.Chmod(dir, 0777)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	binPath := "/usr/local/bin"
 
 	err = os.MkdirAll(path.Join(dir, binPath), 0777)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	err = os.Link(path.Join(binPath, "docker-explorer"), path.Join(dir, binPath, "docker-explorer"))
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	err = syscall.Chroot(dir)
-	if err != nil {
-		return err
-	}
-
-	return os.Chdir("/")
+	return dir, nil
 }
